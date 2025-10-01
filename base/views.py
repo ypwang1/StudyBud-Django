2a3065e61ce075c23d55897698899be37786f0de
Yupeng Wang <yupengori@gmail.com>
2025-10-01 20:23:51 +0200

    add log in functionality

--- base/views.py
+++ base/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.db.models import Q
 from django.http import HttpResponse
 from .models import Room, Topic
@@ -11,6 +14,23 @@ from .forms import RoomForm
 # ]
 
 def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password =password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR password does not exist')
+
     context = {}
     return render(request, 'base/login_register.html', context)
 
