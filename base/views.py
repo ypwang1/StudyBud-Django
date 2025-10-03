c76d1cbca36ab799a70408959ee4454a7893d999
Yupeng Wang <yupengori@gmail.com>
2025-10-03 22:17:48 +0200

    add profile

--- base/views.py
+++ base/views.py
@@ -67,7 +67,7 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
-    room_messages = Message.objects.all()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
     context = {'rooms': rooms, "topics": topics, "room_count": room_count, 'room_messages': room_messages}
     return render(request,'base/home.html', context)
@@ -87,6 +87,12 @@ def room(request, pk):
 
     context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    context = {'user': user}
+    return render(request, 'base/profile.html', context)
+
 
 @login_required(login_url='login')
 def createRoom(request):
