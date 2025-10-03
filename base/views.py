8fcda54b7bd6a95fca04901df698a7764bea1777
Yupeng Wang <yupengori@gmail.com>
2025-10-03 21:49:42 +0200

    add side activity bar

--- base/views.py
+++ base/views.py
@@ -67,7 +67,9 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
-    context = {'rooms': rooms, "topics": topics, "room_count": room_count}
+    room_messages = Message.objects.all()
+
+    context = {'rooms': rooms, "topics": topics, "room_count": room_count, 'room_messages': room_messages}
     return render(request,'base/home.html', context)
 
 def room(request, pk):
@@ -121,3 +123,13 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': room})
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here!')
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': message})
