c951aad00767666a5eca3bef8c5f78315888452d
Yupeng Wang <yupengori@gmail.com>
2025-10-04 22:42:14 +0200

    add styling

--- studybud/settings.py
+++ studybud/settings.py
@@ -117,7 +117,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# STATIC_ROOT=
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
